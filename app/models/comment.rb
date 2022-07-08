e937e7e837a2c8f974ea56feebc1fd162e260ed7
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-08 06:26:06 +0500

    add validation and test

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post, class_name: 'Post'
 
+  validates :text, presence: true, length: { in: 3..250 }
+
   after_save :update_comments_counter
 
   def update_comments_counter
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
+
   after_save :update_posts_counter
 
   def update_posts_counter
@@ -10,6 +15,6 @@ class Post < ApplicationRecord
   end
 
   def last_five_comments
-    comments.includes(:post).order(created_at: :ASC).limit(5)
+    comments.includes(:post).order(created_at: :DESC).limit(5)
   end
 end
