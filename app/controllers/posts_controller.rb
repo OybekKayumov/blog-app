1af148bc5e14f191a29dae2a17bd8ef70f4d162a
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-14 05:55:06 +0500

    fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,5 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
-  end  
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
-class UsersController < ApplicationController  
+class UsersController < ApplicationController
   def index
     @user = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    @post = @user.last_three_posts    
+    @post = @user.last_three_posts
   end
 end
