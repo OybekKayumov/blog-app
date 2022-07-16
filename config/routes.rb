0c78c68ff94772ba778fc77aba01aa6a1fbd8567
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-16 16:16:06 +0500

    update form

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new] do
       resources :comments, only: [:create]
+      resources :likes, only: [:create]
     end   
   end
-
-
-
-  # post '/users/:user_id/posts', to: 'posts#create', as: 'create_user_post'
-  # post '/users/:user_id/posts/:id/likes', to: 'likes#create', as: 'create_post_like'
-  # get '/posts/:post_id/comments/new', to: 'comments#new', as: 'new_post_comment'
-  # post '/posts/:post_id/comments', to: 'comments#create', as: 'create_post_comment'
+  
 end
