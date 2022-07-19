39767bbe25b9c2c0ac2ac4b561908fc3f9c776a1
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-07-19 18:28:15 +0300

    Login page, signup, and controller updated

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # root "users#index"
-  root to: "home#index"
-
+  root to: "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new] do
       resources :comments, only: [:create]
