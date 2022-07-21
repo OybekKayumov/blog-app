aec00610425bcddb82d071349791d86b75182f33
Achilles-Dev <solohag92@gmail.com>
2022-07-21 20:20:06 +0000

    Update post_controller.rb file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,12 +32,11 @@ class PostsController < ApplicationController
 
   def destroy
     post = Post.find(params[:id])
-    # user = User.find(post.author_id)
-    # user.posts_counter -= 1
+    user = User.find(post.author_id)
+    user.posts_counter -= 1
     post.destroy
-    # user.save
+    user.save
     flash[:success] = 'You have deleted this post successfully!'
-    # redirect_to user_path(current_user.id)
-    authorize! :destroy, @post
+    redirect_to user_path(current_user.id)
   end
 end
