b51e7861e3ff5b9d13e873ee84e50c7644f9993b
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-21 05:41:36 +0500

    update show specs

--- spec/views/users/users_show_spec.rb
+++ spec/views/users/users_show_spec.rb
@@ -1,45 +1,64 @@
 require 'rails_helper'
-RSpec.feature 'User #Show', type: :feature do
-  background do
-    visit new_user_session_path
-    @user1 = User.create(name: 'Sam', bio: 'This is my bio',
-                         photo: 'https://steelfoads/', email: 'Sam@gmail.com', password: 'pard', confirmed_at: Time.now)
 
-    within 'form' do
-      fill_in 'Email', with: @user1.email
-      fill_in 'Password', with: @user1.password
+RSpec.describe 'UserShowPages', type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+  before(:each) do
+    @user = create(:user, name: 'James')
+    login_as @user
+    @other_user = create(:user, name: 'Jane')
+    (1..6).each do |n|
+      post = create(:post, user_id: @other_user.id, title: "Post #{n}")
+      post.update_posts_counter(@other_user)
     end
-    click_button 'Log in'
+    visit "/users/#{@other_user.id}"
   end
 
-  scenario 'show number of posts per user' do
-    Post.create(title: 'This is post 1', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                author_id: @user1.id)
-    Post.create(title: 'This is post 2', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                author_id: @user1.id)
+  describe 'Page content' do
+    it "should have other user's profile picture" do
+      expect(page).to have_css("img[src*='#{@other_user.photo}']")
+    end
+    it 'should have username of other user' do
+      expect(page).to have_content(@other_user.name)
+    end
+    it 'should have number of posts of other user' do
+      expect(page).to have_content("Number of posts: #{@other_user.posts_counter}")
+    end
+    it "should display other user's bio" do
+      expect(page).to have_content(@other_user.bio)
+    end
 
-    find("a[href='#{user_path(@user1.id)}']").click
-    expect(page).to have_content 'Number of posts:'
+    context "Other user's first three posts" do
+      it 'should display first three posts of other user' do
+        expect(page).to have_content(@other_user.posts[0].title)
+      end
+      it 'should display first three posts of other user' do
+        expect(page).to have_content(@other_user.posts[1].title)
+      end
+      it 'should display first three posts of other user' do
+        expect(page).to have_content(@other_user.posts[2].title)
+      end
+      it 'should display first three posts of other user' do
+        expect(page).to_not have_content(@other_user.posts[3].title)
+      end
+    end
+
+    it 'should display a button for showing all the other users posts' do
+      expect(page).to have_content('See all posts')
+    end
   end
 
-  scenario "show user's bio." do
-    Post.create(title: 'This is post 3', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                author_id: @user1.id)
+  describe 'Interactions' do
+    it "should redirect to other user's clicked post's show page" do
+      first_post = @other_user.posts.first
+      click_link(first_post.id.to_s)
+      expect(page).to have_current_path("/users/#{@other_user.id}/posts/#{first_post.id}")
+    end
 
-    find("a[href='#{user_path(@user1.id)}']").click
-    expect(page).to have_content 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-  end
-
-  scenario "show user's first 3 posts." do
-    Post.create(title: 'Post 4', text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco ',
-                author_id: @user1.id)
-    Post.create(title: 'Post 5', text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco ',
-                author_id: @user1.id)
-    Post.create(title: 'Post 6', text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco ',
-                author_id: @user1.id)
-    Post.create(title: 'Post 7', text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco ',
-                author_id: @user1.id)
-    find("a[href='#{user_path(@user1.id)}']").click
-    expect(page).to_not have_content 'Excepteur sint occaecat cupidatat non proident'
+    it "should redirect to other user's posts index page" do
+      click_link 'See all posts'
+      expect(page).to have_current_path("/users/#{@other_user.id}/posts")
+    end
   end
 end
