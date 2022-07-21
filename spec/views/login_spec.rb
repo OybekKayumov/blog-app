4c61c269619be531db64c7164c9095c7a77b89d0
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-21 07:24:12 +0500

    update login specs

--- spec/views/login_spec.rb
+++ spec/views/login_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
-require 'login_partial'
+# require 'login_partial_spec.rb'
+
 RSpec.feature 'Login', type: :feature do
   before :each do
     User.create(name: 'Testing', posts_counter: 0, email: 'user@example.com', password: 'password',
                 confirmed_at: '2022-03-02 22:25:13.71382')
   end
-  include Login
+  # include login_partial
   it 'can enter a name and receive a greeting' do
     visit user_session_path
     expect(page).to have_content 'Log in'
@@ -20,5 +21,25 @@ RSpec.feature 'Login', type: :feature do
     visit user_session_path
     click_button 'Log in'
     expect(page).to have_content 'Invalid Email or password.'
+  end
+
+  it 'click the login button with wrong inputs ' do
+    visit 'users/sign_in'
+    within('form') do
+      fill_in 'user[email]', with: 'incorrect@incorrect'
+      fill_in 'user[password]', with: 'incorrect'
+    end
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Email or password.'
+  end
+
+  it 'Login successfully' do
+    visit user_session_path
+    within('form') do
+      fill_in 'user[email]', with: 'user@example.com'
+      fill_in 'user[password]', with: 'password'
+    end
+    click_button 'Log in'
+    expect(current_path).to eq root_path
   end
 end
