41517047bc80cc48f7f7dbf74289da75f3e9c5bd
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-07-22 14:24:59 +0300

    Rubocop error fixed

--- app/controllers/api_endpoints_controller.rb
+++ app/controllers/api_endpoints_controller.rb
@@ -1,11 +1,12 @@
 class ApiEndpointsController < ApplicationController
-  def user_posts    
+  def user_posts
     @user = User.find(params[:user_id])
-    @posts = @user.posts 
-    render json: @posts, status: :ok    
-  end   
+    @posts = @user.posts
+    render json: @posts, status: :ok
+  end
+
   def user_post_comments
-   @comments = Comment.find_by(post_id: params[:post_id])
+    @comments = Comment.find_by(post_id: params[:post_id])
     render json: @comments, status: :ok
   end
 end
