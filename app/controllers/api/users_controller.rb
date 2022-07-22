25be5aa3696970267b52793fe62617a76c427a34
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-22 14:50:26 +0500

    add jwt and api\user-controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users, status: :ok
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user, status: :ok
+  end
+end
